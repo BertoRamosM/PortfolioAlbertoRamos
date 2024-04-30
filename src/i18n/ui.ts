afd6605ca09ee3677798eed289050bed0a24d934
ARamos <albertramos902@gmail.com>
2024-04-30 15:52:40 +0100

    about me translated

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,23 @@ export const ui = {
     "header.about": "About",
     "header.contact": "Contact",
     "header.language": "Languages",
+    "about.1": "I'm Alberto, a Nomad Web Developer!",
+    "about.2": `For years, I wandered the globe, exploring far-off lands and diving into diverse
+    professions. Little did I know that my true calling awaited me in the unlikeliest
+    of places – `,
+    "about.3": `the world of programming.`,
+    "about.4": ` About a year ago, I stumbled upon the amazing world of web development.
+      What started as a chance encounter swiftly blossomed into a burning
+      passion and a newfound career path, blended with an exciting van life.`,
+    "about.5": `Now, I'm on the search for my`,
+    "about.6": ` first job as a web developer`,
+    "about.7": `, eager to establish myself and continue growing and learning in this
+      incredible industry.`,
+    "about.8": ` While I acknowledge that this is merely the beginning of my journey,`,
+    "about.9": `with unwavering dedication and perseverance, I will succeed.`,
+    "about.10": `Currently passionate about learning`,
+    "about.11": ` backend development with Node.js and Express.`,
+    "about.12": `Join me on this adventure!`,
   },
   es: {
     "header.education": "Educación",
@@ -21,6 +38,19 @@ export const ui = {
     "header.about": "About",
     "header.contact": "Acerca de mí",
     "header.language": "Idiomas",
+    "about.1": "¡Soy Alberto, un desarrollador web nómada!",
+    "about.2": `Durante años, vagué por el mundo, explorando tierras lejanas y sumergiéndome en la diversas profesiones. No sabía que mi verdadera vocación me esperaba en el momento más improbable.
+ de lugares -`,
+    "about.3": ` el mundo de la programación.`,
+    "about.4": `Hace aproximadamente un año, tropecé con el asombroso mundo del desarrollo web. Lo que comenzó como un encuentro fortuito rápidamente se convirtió en una pasión ardiente y una nueva carrera profesional, combinada con una emocionante vida en furgoneta.`,
+    "about.5": `Ahora, estoy en búsqueda de mi`,
+    "about.6": ` primer trabajo como desarrollador web`,
+    "about.7": `, motivado por establecerme y seguir creciendo y aprendiendo en esta increíble industria.`,
+    "about.8": ` Aunque reconozco que esto es simplemente el comienzo de mi viaje,`,
+    "about.9": `con una dedicación y perseverancia inquebrantables, lograré el éxito.`,
+    "about.10": `Actualmente apasionado por aprender`,
+    "about.11": ` desarrollo del backend con Node.js y Express.`,
+    "about.12": `¡Únete a mí en esta aventura!`,
   },
   cat: {
     "header.education": "Educació",
@@ -28,6 +58,19 @@ export const ui = {
     "header.about": "Sobre mi",
     "header.contact": "Contacte",
     "header.language": "Idiomes",
+    "about.1": "Sóc l'Albert, un desenvolupador web nòmada!",
+    "about.2": `Durant anys, vaig vagar pel món, explorant terres llunyanes i submergint-me en la diverses professions. No sabia que la meva veritable vocació m'esperava en el més improbable
+ de llocs -`,
+    "about.3": ` el món de la programació.`,
+    "about.4": `Fa aproximadament un any, vaig descobrir el meravellós món del desenvolupament web. El que va començar com un encontre fortuït ràpidament va esdevenir una passió ardenta i un nou camí professional, combinat amb una emocionant vida en furgoneta.`,
+    "about.5": `Ara, estic a la recerca de la meva`,
+    "about.6": ` primer treball com a desenvolupador web`,
+    "about.7": `, amb moltes ganes d'establir-me i continuar creixent i aprenent en aquesta increïble indústria.`,
+    "about.8": ` Tot i que reconec que això és simplement l'inici del meu camí,`,
+    "about.9": `amb una dedicació i una perseverança inquebrantables, aconseguiré l'èxit.`,
+    "about.10": `Actualment apassionat per aprendre`,
+    "about.11": ` desenvolupament del backend amb Node.js i Express.`,
+    "about.12": `Uneix-te a mi en aquesta aventura!`,
   },
   fr: {
     "header.education": "Éducation",
@@ -35,5 +78,18 @@ export const ui = {
     "header.about": "À propos",
     "header.contact": "Contact",
     "header.language": "Langages",
+    "about.1": "Je m'appelle Alberto, développeur web nomade !",
+    "about.2": `Pendant des années, j'ai parcouru le monde, explorant des terres lointaines et plongeant dans diverses professions Je ne savais pas que ma véritable vocation m'attendait dans les circonstances les plus improbables.
+ de lieux –`,
+    "about.3": " le monde de la programmation.",
+    "about.4": `Il y a environ un an, j'ai découvert l'incroyable monde du développement web. Ce qui a commencé comme une rencontre fortuite s'est rapidement transformé en une passion ardente et une nouvelle voie professionnelle, combinée à une excitante vie en van.`,
+    "about.5": `Maintenant, je suis à la recherche de mon `,
+    "about.6": `premier emploi en tant que développeur web`,
+    "about.7": `, désireux de m'établir et de continuer à grandir et apprendre dans cette incroyable industrie.`,
+    "about.8": ` Bien que je reconnaisse que c'est simplement le début de mon voyage,`,
+    "about.9": `avec une dédication et une persévérance inébranlables, je réussirai.`,
+    "about.10": `Actuellement passionné par l'apprentissage`,
+    "about.11": ` développement backend avec Node.js et Express.`,
+    "about.12": `Rejoignez-moi dans cette aventure !`,
   },
 } as const;
