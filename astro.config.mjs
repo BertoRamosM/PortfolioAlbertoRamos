614be74c6e2d41c36734adb49a087b9d7cbf345e
ARamos <albertramos902@gmail.com>
2024-05-26 14:23:08 +0100

    added small animation to header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'cat', 'fr'],
+    locales: ['en', 'es', 'ca', 'fr'],
     routing: {
       prefixDefaultLocale: false
     }
