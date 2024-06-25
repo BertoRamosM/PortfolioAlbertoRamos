2e3ab6bcb1ac2601057ead42345a87436710252c
ARamos <albertramos902@gmail.com>
2024-06-25 09:12:47 +0000

    adding transitions in the blog cards and single page

--- src/blogComponents/BlogCard.jsx
+++ src/blogComponents/BlogCard.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import Tags from "./Tags";
 
 const BlogCard = ({ article }) => {
+  useEffect(() => {
+    const image = document.querySelector(".article-image");
+    if (image) {
+      image.style.viewTransitionName = "article-image";
+    }
+    const title = document.querySelector(".article-title");
+    if (title) {
+      title.style.viewTransitionName = "article-title";
+    }
+  }, []);
+
   return (
     <div className="mt-6 py-4 rounded-xl border-2 border-white/40 text-white relative overflow-hidden group flex flex-col justify-between gap-4 w-[90%] sm:w-full text-pretty p-6 bg-transparent">
       <div className="p-6">
         <div className="flex items-center justify-between flex-col sm:flex-row pb-4 sm:pb-0">
-          <h2 className="text-lg sm:text-2xl font-semibold mb-2 flex items-end border-b-2 border-purple-400">
+          <h2 className="text-lg sm:text-2xl font-semibold mb-2 flex items-end border-b-2 border-purple-400 article-title">
             <a
               href={`/blog/${article.slug}`}
               className="group-hover:text-purple-500 cursor-pointer transition-all duration-100"
@@ -18,7 +30,7 @@ const BlogCard = ({ article }) => {
             width={200}
             height={200}
             alt={article.data.title}
-            className="rounded-lg"
+            className="rounded-lg article-image"
           />
         </div>
         <p className="text-gray-300 text-sm mb-4 px-2">
